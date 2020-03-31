9941e956ce1f48260361f469d051e85b46f3bfd9
Laura Pérez Mayos <lpmayos@gmail.com>
2020-03-31 13:04:12 +0200

    fixed suspicious bug; refactored to clarify code

--- examples/modeling_bert.py
+++ examples/modeling_bert.py
@@ -12,73 +12,71 @@ logger = logging.getLogger(__name__)
 
 
 class BertForParsing(BertPreTrainedModel):
+    """ Implements Dozat&Maning DEEP BIAFFINE ATTENTION FOR NEURAL DEPENDENCY PARSING
+    https://arxiv.org/pdf/1611.01734.pdf
+    """
+
     def __init__(self, config, mlp_dim, num_labels):
         super(BertForParsing, self).__init__(config)
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.mlp_dim = mlp_dim
-
-        self.mlp_head_arc = nn.Linear(config.hidden_size, mlp_dim)
-        self.mlp_dep_arc = nn.Linear(config.hidden_size, mlp_dim)
-        self.biaffine_arc = BiAffine(mlp_dim, 1)
-
-        self.mlp_head_label = nn.Linear(config.hidden_size, mlp_dim)
-        self.mlp_dep_label = nn.Linear(config.hidden_size, mlp_dim)
-        self.biaffine_label = BiAffine(mlp_dim, num_labels)
         self.num_labels = num_labels
+
+        self.mlp_arc_head = nn.Linear(config.hidden_size, mlp_dim)  # Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
+        self.mlp_arc_dep = nn.Linear(config.hidden_size, mlp_dim)
+        self.biaffine_classifier_arcs = BiAffine(mlp_dim, 1)
+
+        self.mlp_label_head = nn.Linear(config.hidden_size, mlp_dim)
+        self.mlp_label_dep = nn.Linear(config.hidden_size, mlp_dim)
+        self.biaffine_classifier_labels = BiAffine(mlp_dim, num_labels)
 
         self.init_weights()
 
-    def decode(self, scores):
-        # do viterbi decoding, build graph for every instance
+    def forward(self,
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,
+                heads=None,
+                labels=None):
 
-        pass
-
-    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None,
-                position_ids=None, head_mask=None, inputs_embeds=None, heads=None, labels=None):
-        outputs = self.bert(input_ids,
-                            attention_mask=attention_mask,
-                            token_type_ids=token_type_ids,
-                            position_ids=position_ids,
-                            head_mask=head_mask,
-                            inputs_embeds=inputs_embeds)
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds
+        )
 
         sequence_output = outputs[0]
-
         sequence_output = self.dropout(sequence_output)
-        s_head_arc = self.mlp_head_arc(sequence_output)
-        s_dep_arc = self.mlp_dep_arc(sequence_output)
 
-        s_head_label = self.mlp_head_label(sequence_output)
-        s_dep_label = self.mlp_head_label(sequence_output)
-
-        # print("s_head", s_head)
-        # print("s_dep", s_dep)
-
-        # do the mask
-        # attention_mask [batch_size, seq_len]
-
-        # attention_mask = attention_mask.unsqueeze(-1).expand(attention_mask.shape + (attention_mask.size(-1),))
-
-        logits_arc = self.biaffine_arc(s_head_arc, s_dep_arc)  # [batch_size, seq_len, seq_len]
-
+        # Dozat&Manning: "rather than using the top recurrent states of the
+        #   LSTM in the biaffine transformations, we first put them
+        #   through MLP operations that reduce their dimensionality."
+        s_arc_head = self.mlp_arc_head(sequence_output)
+        s_arc_dep = self.mlp_arc_dep(sequence_output)
+        logits_arc = self.biaffine_classifier_arcs(s_arc_head, s_arc_dep)  # [batch_size, seq_len, seq_len]
         logits_arc = logits_arc.transpose(-1, -2)
 
-        logits_label = self.biaffine_label(s_head_label, s_dep_label)  # [batch_size, num_labels, seq_len, seq_len]
-
+        s_label_head = self.mlp_label_head(sequence_output)
+        s_label_dep = self.mlp_label_dep(sequence_output)  # TODO lpmayos: I change this to mlp_label_dep
+        logits_label = self.biaffine_classifier_labels(s_label_head, s_label_dep)  # [batch_size, num_labels, seq_len, seq_len]
         logits_label = logits_label.transpose(-1, -3)  # [batch_size, seq_len, seq_len, num_labels]
 
         preds = torch.argmax(logits_arc, dim=1).unsqueeze(-1)  # [batch_size, seq_len, 1]
-
         indices = preds.unsqueeze(-1).expand(preds.shape + (self.num_labels,))  # [batch_size, seq_len, 1 , num_labels]
-
-        # print("logits_label_shape", logits_label.shape)
-        # print("indices shape", indices.shape)
         logits_label = torch.gather(logits_label, -2, indices).squeeze(-2)  # [batch_size, seq_len,num_labels]
 
         outputs = (logits_arc, logits_label) + outputs[2:]
+
         if heads is not None and labels is not None:
             loss_fct = CrossEntropyLoss()
+
             logits_arc = logits_arc.contiguous().view(-1, logits_arc.size(-1))
             heads = heads.view(-1)
             loss = loss_fct(logits_arc, heads)
@@ -86,11 +84,7 @@ class BertForParsing(BertPreTrainedModel):
             logits_label = logits_label.contiguous().view(-1, self.num_labels)
             labels = labels.view(-1)
             loss_labels = loss_fct(logits_label, labels)
-            # print("loss heads", loss)
-            # print("loss labels", loss_labels)
-            # print("loss", loss)
-            # print("preds", preds)
-            # print("labels", labels)
+
             loss = loss + loss_labels
             outputs = (loss,) + outputs
 
@@ -112,3 +106,80 @@ class BiAffine(nn.Module):
         Rd = Rd.unsqueeze(1)
         S = Rh @ self.U @ Rd.transpose(-1, -2)
         return S.squeeze(1)
+
+
+class BertForSRL(BertPreTrainedModel):
+
+    def __init__(self, config):
+        super(BertForSRL, self).__init__(config)
+        self.num_labels = config.num_labels
+        self.num_BIO_labels = 3
+        self.num_CRO_labels = 3
+        self.num_SRL_labels = 22
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+        self.BIO_classifier = nn.Bilinear(config.hidden_size, 1, self.num_BIO_labels)
+        self.CRO_classifier = nn.Bilinear(config.hidden_size, self.num_BIO_labels, self.num_CRO_labels)
+        self.SRL_classifier = nn.Bilinear(config.hidden_size, self.num_CRO_labels, self.num_SRL_labels)
+        self.label_classifier = nn.Bilinear(config.hidden_size, self.num_SRL_labels, self.num_labels)
+        # self.classifier = nn.Linear(config.hidden_size, self.num_labels)
+
+        self.init_weights()
+
+    def forward(self, input_ids=None, verb_seq_ids=None, label_BIO_ids=None, label_CRO_ids=None, label_SRL_ids=None,
+                attention_mask=None, token_type_ids=None,
+                position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
+
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            position_ids=position_ids,
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds)
+
+        sequence_output = outputs[0]
+
+        sequence_output = self.dropout(sequence_output)
+        # print("sequence_output", sequence_output.shape)
+        # print("verb_seq_ids", verb_seq_ids.shape)
+
+        # verb_logits = self.verb_embedding(verb_seq_ids.unsqueeze(-1).float())
+
+        BIO_logits = self.BIO_classifier(sequence_output, verb_seq_ids.unsqueeze(-1).float())
+        CRO_logits = self.CRO_classifier(sequence_output, BIO_logits)
+        SRL_logits = self.SRL_classifier(sequence_output, CRO_logits)
+
+        logits = self.label_classifier(sequence_output, SRL_logits)
+
+        # print("logits shape", logits.shape)
+        # print("num_labels", self.num_labels)
+
+        # print("BIO logits shape", BIO_logits.shape)
+        # print("num_BIO_labels", self.num_BIO_labels)
+
+        # print("CRO logits shape", CRO_logits.shape)
+        # print("num_CRO_labels", self.num_CRO_labels)
+
+        # print("SRL logits shape", SRL_logits.shape)
+        # print("num_SRL_labels", self.num_SRL_labels)
+
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            loss_BIO = loss_fct(BIO_logits.view(-1, self.num_BIO_labels), label_BIO_ids.view(-1))
+            loss_CRO = loss_fct(CRO_logits.view(-1, self.num_CRO_labels), label_CRO_ids.view(-1))
+            loss_SRL = loss_fct(SRL_logits.view(-1, self.num_SRL_labels), label_SRL_ids.view(-1))
+            # Only keep active parts of the loss
+            if attention_mask is not None:
+                active_loss = attention_mask.view(-1) == 1
+                active_logits = logits.view(-1, self.num_labels)[active_loss]
+                active_labels = labels.view(-1)[active_loss]
+                loss = loss_fct(active_logits, active_labels)
+            else:
+
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            loss = loss + loss_BIO + loss_CRO + loss_SRL
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), scores, (hidden_states), (attentions)
